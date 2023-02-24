2c6c5424e475e1a290d08920fec1304f816f4d44
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-02-24 23:25:31 +0530

    Add

---
+++ add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+    int a,b,c;
+    printf("Enter the value 1 :- ", a);
+    scanf("%d",&a);
+    printf("Enter the value 2 :- ", b);
+    scanf("%d",&b);
+    c=a+b;
+    printf("Sum of two numbers :- %d", c);
+  
+}
