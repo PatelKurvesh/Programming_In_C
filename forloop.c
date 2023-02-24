22743d625b3ce594cd22336b42e6ba2c4cfb246e
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-02-24 23:28:46 +0530

    forloop

---
+++ forloop.c
@@ -0,0 +1,14 @@
+// for loop example (// Print numbers from 1 to 10)
+#include <stdio.h>
+
+int main() {
+  int i;
+
+  for (i = 1; i < 11; i++)
+  {
+    printf("%d \n", i);
+  }
+  return 0;
+}
+
+
