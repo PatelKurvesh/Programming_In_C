1bf3c47a15ef681521137b27ca8344b4100e24ed
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-02-28 17:55:31 +0530

    ifelse

---
+++ ifelse.c
@@ -0,0 +1,15 @@
+//odd even numbers using if else.
+
+#include<stdio.h>
+main()
+{
+    int a;
+    printf("Enter the value :- ");
+    scanf("%d", &a);
+    if(a%2==0){
+        printf("even number");
+    }
+    else{
+        printf("odd");
+    }
+}
